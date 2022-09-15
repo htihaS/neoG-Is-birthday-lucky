44375f0ff1038f875ac8514d7c5e16a5be19b1ac
= <=>
2022-09-15 15:59:08 +0530

    negetive input handled

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ check.addEventListener("click",()=>{
     for(var i=0;i<8;i++){
         sum += Number(newdob.charAt(i));
     }
-    if(sum%luckynumber.value===0){
-        output.innerText = "You are lucky brooo"
+    if(luckynumber.value >0){
+        if(sum%luckynumber.value===0){
+            output.innerText = "Your birthday is  lucky! "
+        }else{
+            output.innerText = "Your birthday is not lucky"
+        }
     }else{
-        output.innerText = "You suck!"
+        output.innerText = "Enter a lucky number greater than zero"
     }
 })
